9783c5ff7803881d3511bd0104e179029bd1a7e1
Scott Hall <62564304+scott-a-hall@users.noreply.github.com>
2020-04-13 15:23:42 -0500

    add Arcanine and Diglett objects to repository array

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ var pokedexRepository = [
     name: 'Charmander',
     height: 2\' 0",
     types: ['fire'],
-  }
+  },
+  {
+    name: 'Arcanine',
+    height: 6\' 3",
+    types: ['fire'],
+  },
+  {
+    name: 'Diglett',
+    height: 0\' 08",
+    types: ['ground'],
+  },
 ]
