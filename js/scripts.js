f75ed69e904e60e826d7cb53f818836ff7909150
Scott Hall <62564304+scott-a-hall@users.noreply.github.com>
2020-04-14 14:45:56 -0500

    change format of all heights to decimals

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,37 @@
 var pokedexRepository = [
   {
     name: 'Charmander',
-    height: 2\' 0",
+    height: 2.0,
     types: ['fire'],
   },
   {
     name: 'Arcanine',
-    height: 6\' 3",
+    height: 6.3,
     types: ['fire'],
   },
   {
     name: 'Diglett',
-    height: 0\' 08",
+    height: .08,
     types: ['ground'],
   },
   {
     name: 'Marowak',
-    height: 3\' 03",
+    height: 3.03,
     types: ['ground'],
   },
   {
     name: 'Nidoqueen',
-    height: 4\' 03",
+    height: 4.03,
     types: ['poison', 'ground'],
   },
   {
     name: 'Butterfree',
-    height: 3\' 07",
+    height: 3.07,
     types: ['bug', 'flying'],
   },
   {
     name: 'Pidgeotto',
-    height: 3\' 07",
+    height: 3.07,
     types: ['normal', 'flying'],
   },
 ]
